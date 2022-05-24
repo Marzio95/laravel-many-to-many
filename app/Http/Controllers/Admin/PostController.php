0d8cbb4070ed0987cd9e49b08704e616cd656a8a
Marzio95 <marziodellarocca14@gmail.com>
2022-05-24 15:58:53 +0200

    first commit

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,144 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Category;
+use App\Http\Controllers\Controller;
+use App\Post;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class PostController extends Controller
+{
+    public function myindex()
+    {
+        $posts = Post::where('user_id', Auth::user()->id)->paginate(20);
+        return view('admin.posts.index', compact('posts'));
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+
+        $posts = Post::where('title', 'LIKE', "%$request->s%")
+                        ->where('category_id', $request->category)
+                        ->where('user_id', $request->author)
+                        ->paginate(20);
+
+        $categories = Category::all();
+        $users = User::all();
+
+        $posts = Post::paginate(20);
+        return view('admin.posts.index', [
+            'posts' => $posts,
+            'categories' => $categories,
+            'users' => $users,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $categories = Category::all();
+        return view('admin.posts.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|unique:posts|max:20',
+            'postText' => 'required|max:500',
+            'slug' => 'required',
+            'category_id' => 'required|exists:App\Category,id',
+        ]);
+
+        $formData = $request->all() + ['user_id' => Auth::user()->id];
+        $newPost = Post::create($formData);
+        return redirect()->route('admin.posts.show', $newPost->slug);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Post $post)
+    {
+        return view('admin.posts.show', [
+            'pageTitle' => $post->title,
+            'post' => $post,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Post $post)
+    {
+        if (Auth::user()->id === $post->user_id) {
+            return view('admin.posts.edit', compact('post'));
+        }
+        abort(403);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Post $post)
+    {
+        $request->validate([
+            'title' => 'required|max:20',
+            'postText' => 'max:500',
+            'slug' => ['required', Rule::unique('posts')->ignore($post)],
+        ]);
+
+        $formData = $request->all();
+        $post->update($formData);
+        return redirect()->route('admin.posts.show', $post->slug);
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Post $post)
+    {
+        if (Auth::user()->id !== $post->user_id) abort(403);
+
+        $post->delete();
+
+        if (url()->previous() === route('admin.posts.edit', $post->slug)) {
+            return redirect()->route('admin.home')->with('status', "Post $post->title deleted");
+        }
+        if (url()->previous() === route('admin.posts.show', $post->slug)) {
+            return redirect()->route('admin.home')->with('status', "Post $post->title deleted");
+        }
+        return redirect(url()->previous())->with('status', 'Profile updated!');
+    }
+}
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,83 @@
+@extends('admin.templates.base')
+@section('pageTitle')
+    Posts
+@endsection
+
+@section('pageMain')
+
+    <body>
+        @if (session('deletd'))
+            <div class="alert alert-warning">{{ session('deletd') }}</div>
+        @endif
+
+        @if (session('status'))
+            <div class="alert alert-success">{{ session('status') }}</div>
+        @endif
+        <div class="text-center">
+            <h3>Filtra i Posts</h3>
+            <form action="" method="get">
+                <div>
+                    <label for="search-string"></label>
+                </div>
+            </form>
+        </div>
+
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Text</th>
+                    <th scope="col">Categoria</th>
+                </tr>
+            </thead>
+
+            <tbody class="table-group-divider">
+                @foreach ($posts as $post)
+                    <tr>
+                        <th scope="row">{{ $post->id }}</th>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->category->name }}</td>
+                        <td>{{ $post->postText }}</td>
+                        <td><a href="{{ route('admin.posts.show', $post->slug) }}">Post</a></td>
+                        <td>
+                            @if (Auth::user()->id === $post->user_id)
+                                <a href="{{ route('admin.posts.edit', $post->slug) }}">Modifica Post</a>
+                            @endif
+                        </td>
+                        <td>
+                            @if (Auth::user()->id === $post->user_id)
+                                <button data-id="{{ $post->slug }}" onclick="event.stopPropagation()"
+                                    class="my_btn_link btn-delete">Elimina Post</button>
+                            @endif
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+
+        </table>
+
+        <div class="text-center d-flex justify-content-center mt-4">
+            {{ $posts->links() }}
+        </div>
+
+        <section id="confirmation-overlay" class="overlay d-none">
+            <div class="popup">
+                <h2>Se continui l'elemento verrà eliminato</h2>
+                <div class="d-flex justify-content-evenly mt-5">
+                    <form method="POST" data-base="{{ route('admin.posts.index') }}">
+                        @csrf
+                        @method('DELETE')
+                        <button onclick="event.stopPropagation()" class="bg-danger text-white p-2 ">ELIMINA
+                            POST</button>
+                    </form>
+                    <button id="btn-no" class="btn bg-primary">NO</button>
+                </div>
+            </div>
+
+        </section>
+
+    </body>
+
+    </html>
+@endsection
