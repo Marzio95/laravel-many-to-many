4a4c0f4424d76cb4e32b1961d4e6ee485c2bbcbd
Marzio95 <marziodellarocca14@gmail.com>
2022-05-24 17:02:32 +0200

    completo le procedure da terminale: creazione model tag, tabella ponte e seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,8 @@ class DatabaseSeeder extends Seeder
         $this->call(UserSeeder::class);
         $this->call(UserInfoSeeder::class);
         $this->call(CategorySeeder::class);
+        $this->call(TagSeeder::class);
         $this->call(PostSeeder::class);
+        $this->call(PostTagSeeder::class);
     }
 }
